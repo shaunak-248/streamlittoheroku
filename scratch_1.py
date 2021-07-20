c8be9cc16e12a4a3e1b9796870d338267eee7377
shaunak-248 <71225014+shaunak-248@users.noreply.github.com>
2021-07-20 20:47:47 +0200

    Update scratch_1.py

--- scratch_1.py
+++ scratch_1.py
@@ -46,7 +46,7 @@ print(class_names)
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('my_model2 (1).hdf5')
+  model=tf.keras.models.load_model('my_model2.hdf5')
   return model
 with st.spinner('Model is being loaded..'):
   model=load_model()
